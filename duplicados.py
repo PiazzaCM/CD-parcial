268619529c97945511648450e7792030651c8aa9
PiazzaCM <carlmalenapiazza@gmail.com>
2024-05-03 11:27:16 -0300

    manejo de errores

--- duplicados.py
+++ duplicados.py
@@ -1,10 +1,16 @@
 #se carga una lista de prueba
 lista = [1,5, 8, 3, 2, 3, 4, 4, 8, 5, 6, 6, 7]
 
-#se define la funcion con el parametro de entrada lista
+#se define la fincion 
 def eliminar_duplicados(lista):
-    #se retorna la lista eliminando los repetidos
-    return list(set(lista))
-
-#se imprime la lista sin los repetidos
+#validaciones de que la lista tenga datos 
+    if not lista: 
+        return 'no hay lista'
+    elif len(lista) == 1: 
+#se retorna la lista
+        return lista
+ #creamos una lista y se borran los duplicados
+    nueva_lista = list(set(lista)) 
+    return nueva_lista
+#mostramos la lista
 print(eliminar_duplicados(lista))
